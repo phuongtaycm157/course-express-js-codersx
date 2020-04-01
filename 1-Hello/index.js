d5f7ff229c87f73d3d85eedee584e8914e7fd04e
Nishi <ngudai999@gmail.com>
2020-04-01 21:54:29 +0700

    Fullfilled lesson 2 template engines

--- 1-Hello/index.js
+++ 1-Hello/index.js
@@ -2,12 +2,35 @@ var express = require('express');
 var app = express();
 var port = 8008;
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', function(request, response) {
-	response.send('<h1>Hello World</h1>');
+	response.render('index', {
+		name: 'Nishi'
+	});
 });
 
 app.get('/users', function(request, response) {
-	response.send('Users List');
+	response.render('users/index', {
+		users: [
+			{
+				firstName: 'John',
+				lastName: 'Doe',
+				email: 'john@example.com'
+			},
+			{
+				firstName: 'Mary',
+				lastName: 'Moe',
+				email: 'mary@example.com'
+			},
+			{
+				firstName: 'July',
+				lastName: 'Dooley',
+				email: 'july@example.com'
+			}
+		]
+	})
 });
 
 app.listen(port, function () {
