4fdc10237729c8f85cb93ffb28c3914fbe53ab5c
Nishi <ngudai999@gmail.com>
2020-04-04 19:42:45 +0700

    Fullfilled lesson 12: Server-side validation

--- 1-Hello/controllers/users.controllers.js
+++ 1-Hello/controllers/users.controllers.js
@@ -21,7 +21,18 @@ module.exports = {
         res.render('users/create');
     },
     postCreate: function(req, res) {
-        user = req.body;
+        var user = req.body;
+        var error = []
+        if (!user.firstName) error.push('First Name');
+        if (!user.lastName) error.push('Last Name');
+        if (!user.email) error.push('Email');
+        if (error.length){
+            res.render('users/create',{
+                user: user,
+                error: error
+            });
+            return;
+        }
         user.id = shortid.generate()
         db.get('users').push(user).write();
         res.redirect('/users');
